5530326c46d3740a3f030476046e78b90f1eb845
smshim <nan1345@naver.com>
2024-11-14 23:34:30 +0900

    second commit

---
+++ shim.js
@@ -0,0 +1,13 @@
+let names = [
+  'steven paul jobs',
+  'bill gates',
+  'zuckerberg',
+  'elon musk',
+  'jeff bezos',
+  'larry pages',
+  'tim cook',
+];
+
+names.forEach((item) => {
+  console.log(item);
+});
